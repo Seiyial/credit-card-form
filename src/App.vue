531e2666b4fbcc09f9eca95bc0c89d60d87bb1a2
Seiyial <seiyial@seiyial.xyz>
2022-11-03 17:20:45 +0800

    V1 (working)

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,131 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<form>
+		<label><span class="nmoncd">Name on Card</span><input name="nameoncard" type="text"
+				placeholder="Lucious B. Byinshtuff"></label>
+		<label><span class="cdnum">Card Number <span>(Dashes not required)</span></span><input name="cardnumber" type="text"
+				placeholder="DONT-USEA-REAL-NMBR"></label>
+		<label class="exp"><span class="expdt">Exp. Date</span><input name="experationdate" type="text"
+				placeholder="MM/YYYY"></label>
+		<label class="cvv"><span class="cvvnum">CVV</span><input name="cvvnumber" type="text" placeholder="XXX"></label>
+		<button type="submit">
+			Buy Now
+		</button>
+	</form>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap");
+
+* {
+	box-sizing: border-box;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+body {
+	display: flex;
+	height: 100vh;
+	justify-content: center;
+	align-items: center;
+	font-size: 1.5em;
+	font-family: Raleway, Helvetica, sans-serif;
+	font-weight: 100;
+	background: linear-gradient(192deg,
+			#ed912f,
+			#f17f2e,
+			#f7493d,
+			#eb0062,
+			#bb0094,
+			#6800bb,
+			#0003c8);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+input {
+	padding: 0.6em 0.25em 0;
+	font-size: 1.5em;
+	height: 2.1em;
+	margin-bottom: 0.25em;
+	border: none;
+	border-bottom: 1px solid peachpuff;
+	font-family: Raleway, Helvetica, sans-serif;
+	font-weight: 100;
+	border-radius: 0.25em;
+}
+
+label span {
+	font-size: 0.75em;
+}
+
+form {
+	width: 100%;
+	max-width: 23em;
+	border: 1px solid #ed912f;
+	padding: 0.75em;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+	align-items: center;
+	background-color: #fff0f0;
+	filter: drop-shadow(-0.25em 0.25em 0.25em rgba(0, 0, 125, 0.25));
+	border-radius: 0.375em;
+}
+
+form label {
+	position: relative;
+}
+
+form label>span {
+	position: absolute;
+	top: 0.25em;
+	left: 0.25em;
+}
+
+.cdnum span {
+	color: #555;
+	font-size: 0.8em;
+}
+
+.cvv {
+	width: 25%;
+}
+
+label,
+input {
+	width: 100%;
+}
+
+.exp {
+	width: calc(75% - 0.75em);
+}
+
+button {
+	width: 100%;
+	height: 2.75em;
+	font-size: 1em;
+	font-family: Raleway, Helvetica, sans-serif;
+	background-color: #e56a9f;
+	border: none;
+	color: #fff;
+	border-radius: 0.25em;
+	margin: 0.25em 0 0 0;
+	padding: 0.5em;
+}
+
+button:hover,
+button:focus {
+	background-color: #eb0062;
+}
+
+::placeholder {
+	color: #ccc;
+	opacity: 1;
+}
+
+::-ms-input-placeholder {
+	color: #ccc;
+}
+
+input:focus {
+	outline-color: #eb0062;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { readFileSync } from 'fs'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [vue()],
+	server: {
+		https: {
+			cert: readFileSync('./localhost.pem'),
+			key: readFileSync('./localhost-key.pem')
+		}
+	}
 })
